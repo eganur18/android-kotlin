ec05de26501fe556ada76591910aa7652e59f21d
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-08-31 14:40:03 +0700

    Melakukan sanity cek

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +19,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hitung() {
-        val berat = binding.beratEditText.text.toString().toFloat()
-        val tinggi = binding.tinggiEditText.text.toString().toFloat() / 100
-        val bmi = berat / (tinggi * tinggi)
 
+        val berat = binding.beratEditText.text.toString()
+        if(TextUtils.isEmpty(berat)) {
+            Toast.makeText(this, "Beratnya berapa sih.", Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggi = binding.tinggiEditText.text.toString()
+        if(TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(this, "Tinggi di isi dulu dong.", Toast.LENGTH_LONG).show()
+            return
+        }
         val selectedId = binding.radioGroup.checkedRadioButtonId
+        if(selectedId == -1) {
+            Toast.makeText(this, "Pilih jenis kelamin ya.", Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggiCm = tinggi.toFloat() / 100
+        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
+
         val isMale = selectedId == R.id.priaRadioButton
         val kategori = getKategori(bmi, isMale)
 
