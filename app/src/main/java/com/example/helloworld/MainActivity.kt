d149eca0a1fb4bf19bdb8a299f395c86d0cb2877
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-08-31 08:28:56 +0700

    Membuat aksi count up

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -14,18 +14,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         haloTextView = findViewById(R.id.haloTextView)
-//        haloTextView.text = "Halo Dunia"
+//        haloTextView.text = "0"
+        var count: Int = 0
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
 //            haloTextView.text = "Assalamu'alaikum"
-            toggleText()
+            count++
+            haloTextView.text = "$count"
+//            toggleText()
         }
     }
-    private fun toggleText() {
-        if(haloTextView.text == "Hello World!")
-            haloTextView.text = "Assalamu'alaikum"
-        else
-            haloTextView.text = "Hello World!"
-    }
+//    private fun toggleText() {
+//        if(haloTextView.text == "Hello World!")
+//            haloTextView.text = "Assalamu'alaikum"
+//        else
+//            haloTextView.text = "Hello World!"
+//        }
+//    }
 }
