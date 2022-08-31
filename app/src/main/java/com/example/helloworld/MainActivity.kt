0222d30c297bf52845818e16c109d84360db9569
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-08-31 09:40:32 +0700

    Menampilkan hewan pertama

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatViewInflater
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +22,12 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Kambing", R.drawable.kambing))
             add(Hewan("Sapi", R.drawable.sapi))
         }
-
-
+        imageView = findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
+        showData(data[0])
+    }
+    private fun showData(hewan: Hewan) {
+        imageView.setImageResource(hewan.gambarResId)
+        textView.text = hewan.nama
     }
 }
