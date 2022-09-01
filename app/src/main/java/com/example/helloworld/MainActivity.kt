9aa6fe33d36ed2df0c33bda46ca0211b80a683df
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-01 11:45:53 +0700

    Belajar tentang lifecycle

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.helloworld.databinding.ActivityMainBinding
@@ -10,29 +13,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val ViewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MVVM", "onCreate dijalankan")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val divider = DividerItemDecoration(this, RecyclerView.VERTICAL)
         binding.recyclerView.addItemDecoration(divider) // membuat pembatas garis list item
-        binding.recyclerView.adapter = MainAdapter(getData())
+        binding.recyclerView.adapter = MainAdapter(ViewModel.data)
         binding.recyclerView.setHasFixedSize(true) //agar tidak berubah ukuran
     }
 
-    private fun getData(): List<Hewan>{
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
-        )
+    override fun onStart() {
+        Log.d("MVVM", "onStart dijalankan")
+        super.onStart()
+    }
+
+    override fun onStop() {
+        Log.d("MVVM", "onStop dijalankan")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("MVVM", "onDestroy dijalankan")
+        super.onDestroy()
     }
 }
