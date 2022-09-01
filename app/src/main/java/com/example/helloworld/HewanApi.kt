1ee756763104153d242bce4ae9f6f016ab16eb8b
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-01 14:40:37 +0700

    Memproses data dari internet

--- app/src/main/java/com/example/helloworld/HewanApi.kt
+++ app/src/main/java/com/example/helloworld/HewanApi.kt
@@ -1,14 +1,20 @@
 package com.example.helloworld
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/" +         //capital semua artinya constanta
         "indraazimi/galeri-hewan/static-api/"                //ini url dari static api json
+
+private val moshi = Moshi.Builder()                         //ini untuk build moshi dibawah
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))                //moshi dilakukan untuk membuat json jd POJO
     .baseUrl(BASE_URL)                                                      //url nya
     .build()
 
@@ -17,7 +23,8 @@ private val retrofit = Retrofit.Builder()
 
 interface HewanApiService { //karena interface punya fungsi untuk menampilkan endpoint ke method
     @GET("static-api.json")           //memanggil end point, kenapa disini hanya potongan url ga url lengkap ? supaya disaat pindah domain, yg diubah hanya BASE_URL diatas
-    suspend fun getHewan(): String //balikan bentuk string
+//    suspend fun getHewan(): String //balikan bentuk string
+    suspend fun getHewan(): List<Hewan> //Karena sudah pakai moshi maka json tadi tinggal di panggil saja
 }
 
 object HewanApi {
--- app/src/main/java/com/example/helloworld/MainViewModel.kt
+++ app/src/main/java/com/example/helloworld/MainViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -25,7 +24,6 @@ class MainViewModel : ViewModel() {
 //            delay(3000L)
 //            data.postValue(initData()) //kalau update background pakai post, tp kalau dr thread utama pakai value
 //        }
-        data.value = initData()
         retrieveDataFromServer() //panggil di init
     }
 
@@ -33,7 +31,8 @@ class MainViewModel : ViewModel() {
         viewModelScope.launch(Dispatchers.IO) {
             try {           //karna ambil data dr internet maka harus ada try
                 val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", result)
+//                Log.d("MainViewModel", result)
+                data.postValue(result)
             } catch (e: Exception) {        //Jika gagal maka tulis juga error nya kenapa
                 Log.d("MainViewModel", "Gagal: ${e.message}")
             }
@@ -41,21 +40,6 @@ class MainViewModel : ViewModel() {
     }
 
     fun getData(): LiveData<List<Hewan>> = data
-
-    private fun initData(): List<Hewan>{
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
-        )
-    }
 
     /*override fun onCleared() {
         Log.d("MVVM", "ViewModel di-cleared")
