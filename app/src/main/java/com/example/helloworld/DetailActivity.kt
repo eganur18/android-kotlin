521bde9d8e8f5b1c5381c442e7181f68a7a9488b
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-02 13:53:41 +0700

    Menambahkan fitur insert

--- app/src/main/java/com/example/helloworld/DetailActivity.kt
+++ app/src/main/java/com/example/helloworld/DetailActivity.kt
@@ -5,10 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
+import com.example.helloworld.database.Diary
+import com.example.helloworld.database.DiaryDb
 import com.example.helloworld.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+
+    private val viewModel: DetailViewModel by lazy {
+        val db = DiaryDb.getInstance(this)
+        val factory = DetailViewModelFactory(db.dao)
+        ViewModelProvider(this, factory)[DetailViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +32,18 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.menuSimpan) {
-            finish()        //untuk kembali ke home atau main activity
+//            finish()        //untuk kembali ke home atau main activity
+            insertDiary()
             return true
         }
         return false
+    }
+
+    private fun insertDiary() {
+        val judul = binding.judulEditText.text.toString()
+        val diary = binding.diaryEditText.text.toString()
+        val data = Diary(judul = judul, diary = diary)
+        viewModel.insertDiary(data)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/helloworld/DetailViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.helloworld
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.helloworld.database.Diary
+import com.example.helloworld.database.DiaryDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class DetailViewModel(private val db: DiaryDao) : ViewModel() {
+    fun insertDiary(diary: Diary){
+        viewModelScope.launch(Dispatchers.IO) {
+            db.insert(diary)
+        }
+    }
+}
+
+class DetailViewModelFactory(private val db: DiaryDao) : ViewModelProvider.Factory {  //membuat factory ketika di viewmodel ada parameter
+    @Suppress("unchecked_cast") //supaya "as" tidak warning
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(db) as T
+        }
+        throw IllegalArgumentException("ViewModel tidak ada")
+    }
+}
