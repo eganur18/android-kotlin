9e1271d65abb906c74ee65e8de655392942cd5b9
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-02 09:43:47 +0700

    Navigasi tampilan main ke detail

--- app/src/main/java/com/example/helloworld/DetailActivity.kt
+++ app/src/main/java/com/example/helloworld/DetailActivity.kt
@@ -1,21 +1,15 @@
 package com.example.helloworld
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.helloworld.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.fab.setOnClickListener {                        //ketika klik fab
-            val intent = Intent(this, DetailActivity::class.java)     //membuat intent ke detail activity
-            startActivity(intent)           //start intent atau membuka ke detail activity
-        }
     }
 }
