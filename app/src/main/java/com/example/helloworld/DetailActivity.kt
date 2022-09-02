1d3f0f70eb64521d7a5f2bf97696bcbc078bcd20
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-02 10:25:43 +0700

    Membuat menu simpan

--- app/src/main/java/com/example/helloworld/DetailActivity.kt
+++ app/src/main/java/com/example/helloworld/DetailActivity.kt
@@ -2,6 +2,9 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.example.helloworld.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -11,5 +14,18 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean { //hanya menampilkan menu saja
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menuSimpan) {
+            finish()        //untuk kembali ke home atau main activity
+            return true
+        }
+        return false
     }
 }
