c6de0c0de10cff09d3e4d00e50553a103d449191
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-02 10:06:11 +0700

    Membuat layout detail

--- app/src/main/java/com/example/helloworld/DetailActivity.kt
+++ app/src/main/java/com/example/helloworld/DetailActivity.kt
@@ -2,14 +2,14 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.helloworld.databinding.ActivityMainBinding
+import com.example.helloworld.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
