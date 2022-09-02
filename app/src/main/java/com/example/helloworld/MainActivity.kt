c4d5e2a0d5a8e146fee90e1f7e8af57d3084f27c
LAPTOP-UQ35QLBF\ASUS <eganur18@gmail.com>
2022-09-02 09:15:46 +0700

    Membuat Layout

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,29 +7,8 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var haloTextView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        haloTextView = findViewById(R.id.haloTextView)
-//        haloTextView.text = "0"
-        var count: Int = 0
-
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-//            haloTextView.text = "Assalamu'alaikum"
-            count++
-            haloTextView.text = "$count"
-//            toggleText()
-        }
     }
-//    private fun toggleText() {
-//        if(haloTextView.text == "Hello World!")
-//            haloTextView.text = "Assalamu'alaikum"
-//        else
-//            haloTextView.text = "Hello World!"
-//        }
-//    }
 }
